f06c3b5456f635e05da5472f8b7b9ad8ad04e6da
adriano alves <adrianobr00@gmail.com>
2020-06-21 13:22:09 -0300

    Initial commit

---
+++ src/main/java/com/algamoney/algamoney/AlgamoneyApplication.java
@@ -0,0 +1,13 @@
+package com.algamoney.algamoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgamoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgamoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algamoney/algamoney/AlgamoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algamoney.algamoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgamoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
